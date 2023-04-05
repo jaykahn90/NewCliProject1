4d192c943e0e419ad95b5eced374c58fb1bb897c
jaykahn90 <107928269+jaykahn90@users.noreply.github.com>
2023-04-05 22:05:53 +1000

    Changed App.tsx to App.js. Also in eslintrc.js include one rule to get rid of errors.

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function App() {
+  return (
+    <View>
+      <Text style={{fontSize: 20}}>Jalal khan</Text>
+    </View>
+  );
+}
+
+export default App;
