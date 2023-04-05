481d0de8ee9e20d2bb5676e265c7b121f93073a0
jaykahn90 <107928269+jaykahn90@users.noreply.github.com>
2023-04-05 22:18:28 +1000

    Learned basic Stack Navigation concept.

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function App() {
+const Stack = createNativeStackNavigator();
+const App = () => {
   return (
-    <View>
-      <Text style={{fontSize: 20}}>Jalal khan</Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+const Home = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 30}}>Home Screen</Text>
     </View>
   );
-}
+};
+
+const Login = props => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text style={{fontSize: 30}}>Login Screen</Text>
+      <Button
+        title="Go to homepage"
+        onPress={() => props.navigation.navigate('Home')}
+      />
+    </View>
+  );
+};
 
 export default App;
