db666b8e403aef8d29ba8da36fb648341b9037bc
jaykahn90 <107928269+jaykahn90@users.noreply.github.com>
2023-04-08 15:41:12 +1000

    pass data btw screens in Stack Navigation

--- Components/Login.js
+++ Components/Login.js
@@ -1,11 +1,20 @@
+import {useState} from 'react';
 import {View, Text, Button, TextInput} from 'react-native';
 export const Login = props => {
+  const [name, setName] = useState('');
+
+  const age = 30;
   return (
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <Text style={{fontSize: 30}}>Login Screen</Text>
+      <TextInput
+        style={{fontSize: 20, borderColor: '#000', borderWidth: 2}}
+        placeholder="enter your name"
+        onChangeText={text => setName(text)}
+      />
       <Button
         title="Go to homepage"
-        onPress={() => props.navigation.navigate('Home')}
+        onPress={() => props.navigation.navigate('Home', {name, age})}
       />
     </View>
   );
