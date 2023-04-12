6d952abe2015eac97aff1db165798e2871c8ed29
jaykahn90 <107928269+jaykahn90@users.noreply.github.com>
2023-04-12 23:08:13 +1000

    Flatlist used for API data

--- App.js
+++ App.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import React, {useEffect, useState} from 'react';
+import {View, Text, FlatList} from 'react-native';
+
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
+  const [data, setData] = useState([]);
+  const getApiData = async () => {
+    const url = 'https://jsonplaceholder.typicode.com/posts';
+    let result = await fetch(url);
+    result = await result.json();
+    setData(result);
+  };
+  useEffect(() => {
+    getApiData();
+  }, []);
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-const Home = () => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30}}>Home Screen</Text>
-    </View>
-  );
-};
-
-const Login = props => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30}}>Login Screen</Text>
-      <Button
-        title="Go to homepage"
-        onPress={() => props.navigation.navigate('Home')}
-      />
+    <View>
+      <Text style={{fontSize: 20, margin: 20}}>Flatlist with API data</Text>
+      {data.length ? (
+        <FlatList
+          data={data}
+          renderItem={({item}) => (
+            <View
+              style={{
+                borderBottomColor: 'grey',
+                borderBottomWidth: 1,
+                padding: 10,
+              }}>
+              <Text
+                style={{fontSize: 24, margin: 20, backgroundColor: 'skyblue'}}>
+                {item.id}
+              </Text>
+              <Text style={{fontSize: 18, margin: 20}}>{item.title}</Text>
+              <Text style={{fontSize: 18, margin: 20}}>{item.body}</Text>
+            </View>
+          )}
+        />
+      ) : null}
     </View>
   );
 };
