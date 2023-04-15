7bf1554233a1e5a642b20f90e393f06e04a9a010
jaykahn90 <107928269+jaykahn90@users.noreply.github.com>
2023-04-15 23:49:46 +1000

    Fetch Data from Json Server

--- App.js
+++ App.js
@@ -1,36 +1,33 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Button} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+  const [data, setData] = useState([]);
+  const getAPIData = async () => {
+    const url = 'http://10.0.2.2:3000/users';
+    let result = await fetch(url);
+    result = await result.json();
+    setData(result);
+  };
+  useEffect(() => {
+    getAPIData();
+  }, []);
 
-const Home = () => {
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30}}>Home Screen</Text>
-    </View>
-  );
-};
-
-const Login = props => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30}}>Login Screen</Text>
-      <Button
-        title="Go to homepage"
-        onPress={() => props.navigation.navigate('Home')}
-      />
+    <View>
+      <Text style={{margin: 20}}>Fetch-data from Json Server</Text>
+      {data.length
+        ? data.map(item => (
+            <View style={{borderWidth: 1, borderColor: 'grey'}}>
+              <Text style={{fontSize: 30, margin: 5}}>Name: {item.name}</Text>
+              <Text style={{fontSize: 30, margin: 5}}>Age: {item.age}</Text>
+              <Text style={{fontSize: 30, margin: 5}}>Email: {item.email}</Text>
+            </View>
+          ))
+        : null}
     </View>
   );
 };
