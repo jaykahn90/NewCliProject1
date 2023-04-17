3faabf45d296e2ab34395a64ec55ba0554a7c58b
jaykahn90 <107928269+jaykahn90@users.noreply.github.com>
2023-04-17 12:38:27 +1000

    Install redux packages and created files

--- App.js
+++ App.js
@@ -1,36 +1,10 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {View, Text} from 'react-native';
 
-const Stack = createNativeStackNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
-
-const Home = () => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30}}>Home Screen</Text>
-    </View>
-  );
-};
-
-const Login = props => {
-  return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 30}}>Login Screen</Text>
-      <Button
-        title="Go to homepage"
-        onPress={() => props.navigation.navigate('Home')}
-      />
+    <View>
+      <Text style={{margin: 20, fontSize: 20}}>Redux Learning</Text>
     </View>
   );
 };
