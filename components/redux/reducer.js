3f5c3093cfd52b2ac0c4f3672d0c7e22ca73d710
jaykahn90 <107928269+jaykahn90@users.noreply.github.com>
2023-04-18 00:02:14 +1000

    Configured reducer for remove cart item

--- components/redux/reducer.js
+++ components/redux/reducer.js
@@ -1,4 +1,4 @@
-import {ADD_TO_CART} from './constants';
+import {ADD_TO_CART, REMOVE_FROM_CART} from './constants';
 
 //define that initial state now
 const initialState = [];
@@ -7,6 +7,13 @@ export const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_CART:
       return [...state, action.data];
+    case REMOVE_FROM_CART:
+      let result = state.filter(item => {
+        return item.name != action.data;
+      });
+
+      return [...result];
+
     default:
       return state;
   }
